6d6ba74d0f23901318dceb866654a0d25342ea94
ShaktiSingh95 <shaktisingh1395@gmail.com>
2016-07-31 17:45:33 +0530

    added model to the app

---
+++ Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ContainerViewSlideMenu
+//
+//  Created by Shakti Pratap Singh on 30/07/16.
+//  Copyright © 2016 Shakti Pratap Singh. All rights reserved.
+//
+
+import Foundation
